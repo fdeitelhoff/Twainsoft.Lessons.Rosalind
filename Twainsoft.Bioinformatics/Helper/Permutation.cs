a2849b7418fde8906cea300fe1be91ba5224a9b1
Fabian Deitelhoff <github@fabiandeitelhoff.de>
2013-02-26 00:06:58 +0100

    Started with the PERM problem.

---
+++ Twainsoft.Bioinformatics/Helper/Permutation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Twainsoft.Bioinformatics.Helper
+{
+    public static class Permutation
+    {
+        public static IEnumerable<IEnumerable<T>> Permutate<T>(IEnumerable<T> list, out int permutationCount)
+        {
+            IEnumerable<IEnumerable<T>> emptyProduct = new[] { Enumerable.Empty<T>() };
+            var permutations = list.Aggregate(
+              emptyProduct,
+              (accumulator, sequence) =>
+                from accseq in accumulator
+                from item in sequence
+                select accseq.Concat(new object[] { item }));
+
+            permutationCount = permutations.Count();
+
+            return permutations;
+        }
+    }
+}
---
+++ Twainsoft.Lessons.Rosalind.Tests/TestCases/HelperTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twainsoft.Bioinformatics.Helper;
+
+namespace Twainsoft.Lessons.Rosalind.Tests.TestCases
+{
+    [TestClass]
+    public class HelperTests
+    {
+        [TestMethod]
+        public void PermutationTest()
+        {
+            // Arrange
+            const int n = 3;
+            var permutate = new List<int>();
+            var permutationCount = 0;
+            const int expectedPermutationCount = 6;
+            
+            // Act
+            for (var i = 1; i < n; i++)
+            {
+                permutate.Add(i);
+            }
+
+            var permutations = Permutation.Permutate(permutate, out permutationCount);
+
+            // Assert
+            Assert.AreEqual(expectedPermutationCount, permutationCount);
+        }
+    }
+}
